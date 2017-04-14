432452b8a9bce41fbc284250ca0d5390dc28b8d6
ELKAISSI SOUHAIL <elkaissimath@gmail.com>
2017-04-14 23:01:00 +0100

    init

---
+++ exe.sh
@@ -0,0 +1,6 @@
+gcc -o out shanks.c  -lm
+clear
+./out
+echo ""
+echo "-------------------------------------------"
+rm out
---
+++ shanks.c
@@ -0,0 +1,7 @@
+#include "include/include.h"
+
+int main(int argv,char **args)
+{
+    printf("hello\n");
+    return 0;
+}
