0aaec3377157010e4219552ce72d13375e5e9363
ELKAISSI SOUHAIL <elkaissimath@gmail.com>
2017-04-15 13:12:57 +0100

    V_0.02

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
  
     Shanks est un language Orienté objet (en cours) ressemblant à python et qui est plus façile à manipuler
     vous pouver avec programmer plus rapidement et creer des UI ...
-    
-    L'extension des fichiers doit etre ".sks"
+
+    L'extension des fichiers doit être ".sks"
 ## Fonctionnalité
 
 - calculer une expression arithmétique simple .
--- shanks.c
+++ shanks.c
@@ -3,10 +3,14 @@
 int main(int argv,char **args)
 {
     printf("\t\tTEST\n");
-    Var *v1 = new_Var("v",1);
-    Var *v2 = new_Var("x",1);
-    Var *v13 = new_Var("r",1);
-    Var *v134 = new_Var("e",1);
     
+    Var *v1 = new_Var("v1",1);
+    Var *v2 = new_Var("x1",1);
+    Var *v13 = new_Var("r1",1);
+    Var *v134 = new_Var("e1",1);
+
+    printf("\n %d",Var_exists("e"));
+    Var_update("r1",14,2);
+    Var_update("x1",-32000,1);
     return 0;
 }
