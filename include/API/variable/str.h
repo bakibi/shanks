e1365ddc187dc878a92912617caf83eedaa51664
ELKAISSI SOUHAIL <elkaissimath@gmail.com>
2017-04-15 15:27:08 +0100

    V0.03

--- include/API/variable/str.h
+++ include/API/variable/str.h
@@ -40,12 +40,42 @@ Str *new_Str(const char *name)
     strcpy(s->value,"");
 
     FILE *f = fopen("strvalues.tmp","a+");
-    fprintf(f,"%s _%s_\n",s->name,s->value);
+    fprintf(f,"%s _\n",s->name);
     fclose(f);
 
     return (Str *)s;
 }//eof
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Cette fonction verifie si un Str existe dans le fichier strvalues.tmp
+int Str_exists(const char *name1)
+{
+    FILE *f = fopen("strvalues.tmp","r");
+    if(!f) return 0;
+    char name[30];
+    char value[1000];
+    while(!feof(f))
+    {
+        fscanf(f,"%s %s\n",name,value);
+        if(strcmp(name1,name) == 0)
+            return 1;
+    }
+    fclose(f);
+    return 0;
+}
 
 
 
--- shanks.c
+++ shanks.c
@@ -5,5 +5,6 @@ int main(int argv,char **args)
     printf("\t\tTEST\n");
     
     Str *s = new_Str("str2");
+    Str_exists("drg");
     return 0;
 }
