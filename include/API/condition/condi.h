8ee427f405d2d44ec282bb40efbc22c370bf3d82
ELKAISSI SOUHAIL <elkaissimath@gmail.com>
2017-04-16 21:29:51 +0100

    v0.04

--- include/API/condition/condi.h
+++ include/API/condition/condi.h
@@ -13,3 +13,110 @@ typedef struct Condition
 } Condition;
 
 
+//      les prototypes des fonction
+Condition *new_Condition(int value);
+Condition *new_Not(Condition *cn);
+Condition *new_Or(Condition *cn1,Condition *cn2);
+Condition *new_And(Condition *c1,Condition *cn2);
+
+//      Les implemetnations des fonctions
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//      Cette fonction cree une nouvelle Condition
+Condition *new_Condition(int value)
+{
+    Condition *cn = (Condition *)malloc(sizeof(Condition));
+    if(!cn) return NULL;
+    cn->type = 1;
+    cn->value = value;
+    return cn;
+}//eof
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//      Cette fonction cree une condition de type Not
+Condition *new_Not(Condition *cn)
+{
+    Condition *c = (Condition *)malloc(sizeof(Condition));
+    if(!c) return NULL;
+    c->type = 0;
+    c->value = 1-cn->value;
+    return c;
+}//eof
+
+
+
+
+
+
+
+
+
+
+
+
+//      Cette fonction cree une condition de type AND
+Condition *new_And(Condition *c1,Condition *cn2);
+{
+    Condition *c = (Condition *)malloc(sizeof(Condition));
+    if(!c) return NULL;
+    c->type = 2;
+    c->value = cn1->value && cn2->value;
+    return c;
+}//eof
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//      Cette fonction cree une condition de type OR
+Condition *new_Or(Condition *c1,Condition *cn2);
+{
+    Condition *c = (Condition *)malloc(sizeof(Condition));
+    if(!c) return NULL;
+    c->type = 3;
+    c->value = cn1->value || cn2->value;
+    return c;
+}//eof
