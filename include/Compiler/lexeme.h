066c4a9e2b1e76155a6763f049b41dcc74ea07d2
ELKAISSI SOUHAIL <elkaissimath@gmail.com>
2017-04-17 21:51:27 +0100

    v0.04

--- include/Compiler/lexeme.h
+++ include/Compiler/lexeme.h
@@ -9,7 +9,6 @@ typedef struct Lexeme
         -1  Value
         0   Nombre
         1   Operation
-        2   Chaine de caractère
         3   Mot clées
         4   (
         5   )
@@ -36,7 +35,9 @@ typedef struct Lexeme
 //      Les prototypes des fonctions
 
 Lexeme *new_Lexeme(char const *l);
-
+int Lexeme_estKey(char const *t);
+int Lexeme_estNum(char const *t);
+const char  *Lexeme_toString(Lexeme *l);
 
 //      Les implementations des fonctions 
 
@@ -49,13 +50,13 @@ Lexeme *new_Lexeme(char const *l);
 
 
 
-
+//  Cette fonction prend en parametre une chaine de caratere et deduit de une lexeme
 Lexeme *new_Lexeme(char const *l)
 {
     Lexeme *lem = (Lexeme *) malloc(sizeof(Lexeme));
     if(!lem) return NULL;
     
-    int taille = sizeof(l);
+    int taille = strlen(l);
     lem->taille = taille;
     lem->value =(char *) malloc(taille);
     strcpy(lem->value,l);
@@ -88,8 +89,82 @@ Lexeme *new_Lexeme(char const *l)
     }//si la taille de la lexeme == 1
     if(taille == 2)
     {
-        
+        if(strcmp(l,"==") == 0 )
+            lem->type = 8;
+        else if(strcmp(l,"!=") == 0 )
+            lem->type = 9;
+        else if(strcmp(l,"<=") == 0 )
+            lem->type = 11;
+        else if(strcmp(l,">=") == 0 )
+            lem->type = 13;
+        else if(strcmp(l,"&&") == 0 )
+            lem->type = 16;
+        else if(strcmp(l,"||") == 0 )
+            lem->type = 17;
     }//si la taille de la lexem == 2
-
+    else
+    {
+        if(Lexeme_estKey(l))
+            lem->type = 3;
+        else if(Lexeme_estNum(l))
+            lem->type = 0;
+        else 
+            lem->type = -1;            
+        
+    }//si la taille est superieur à 2
     return (Lexeme *) lem;
-}//eod
+}//eof
+
+
+
+
+
+
+
+
+//      Cette foction verifie si un str est un mots
+int Lexeme_estKey(char const *t)
+{
+    const char *mots[8] = 
+                                    {
+                                      "var",
+                                      "str",
+                                      "for",
+                                      "while",
+                                      "if",
+                                      "else"
+                                      "switch",
+                                      "case",  
+                                    };
+                                    
+    for(int i=0;i<8;i++)
+        if(strcmp(t,mots[i]) == 0)
+            return 1;
+    return 0;                                    
+}//eof
+
+int Lexeme_estNum(char const *t)
+{
+    int taille = strlen(t);
+    for (int i=0;i<taille;i++)
+        if( !('0'<=t[i] && t[i]<='9') && t[i] !='.' )
+            return 0;
+    return 1;
+}//eof
+
+
+
+const char  *Lexeme_toString(Lexeme *l)
+{
+    char *res = NULL;
+    char tt[2];
+     if(l == NULL) return NULL;
+     res = (char *)malloc(l->taille + 4);
+     sprintf(tt, "%d", l->type);
+     strcpy(res,l->value);
+     strcat(res," ");
+     strcat(res,tt);
+     
+
+     return res;
+}//eof
--- shanks.c
+++ shanks.c
@@ -4,7 +4,8 @@ int main(int argv,char **args)
 {
     printf("\t\tTEST\n");
      char s[30];
-     strcpy(s,"salut tout le \n \n \t \t   sdkfun s  monde");
+     strcpy(s,"var x str 10.5 + - * < >=");
+     
    printf("%s",Str_formate(s));
 
     return 0;
