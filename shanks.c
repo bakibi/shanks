475f49af1587aad00baf710bbc6d625ebcc794a3
ELKAISSI SOUHAIL <elkaissimath@gmail.com>
2017-04-18 22:19:09 +0100

    v0.05

--- shanks.c
+++ shanks.c
@@ -3,18 +3,21 @@
 int main(int argv,char **args)
 {
     printf("\t\tTEST\n");
-     char s[30];
-     strcpy(s,"str str1 = \"reda\"");
+     char s[300];
+     printf("---> ");
+     gets(s);
      Lexemes *liste = Analyseur_LexcialV1(s);
 
    //printf("%s",Str_formate(s));
 
+   printf("\n");
    Lexemes *tmp = liste;
    while(tmp)
    {
        printf("%s\n",Lexeme_toString(tmp->lex));
        tmp = tmp->svt;
    }
+   
    
 
     return 0;
