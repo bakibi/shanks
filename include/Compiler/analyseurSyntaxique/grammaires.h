66424fb3dd6ac8403f892f2efa0a1afa48bdb075
ELKAISSI SOUHAIL <elkaissimath@gmail.com>
2017-04-21 11:04:33 +0100

    v0.07

--- include/Compiler/analyseurSyntaxique/grammaires.h
+++ include/Compiler/analyseurSyntaxique/grammaires.h
@@ -3,6 +3,17 @@
 /*ce fichier contient tout le gramaire possible */
 
 
+
+//  La declaration des variables
+typedef struct decl_var
+{
+    char *type;
+    char *name;
+    char *value;
+}decl_var;
+
+
+
 // la structre de donnees exp_arithmetique 
 typedef struct exp_arith 
 {
@@ -11,6 +22,8 @@ typedef struct exp_arith
     char *droite;
 
 } exp_arith;
+
+
 
 
 
@@ -36,10 +49,19 @@ typedef struct IF
 {
     char *if;
     char *dg;//delimiteur gauche
-    
+    Predicat *predicat; // le predicat 
     char *dd;//delimiteur droite
+    char *exp;
 
 }IF;
+
+
+//  La strucutre de donnes qui contient le ELSE
+typedef struct ELSE
+{
+    char *else; // le else
+   char *exp; // lexpression
+}ELSE ;
 
 
 
