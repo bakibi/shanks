1c819ee3b003ecc6895fc5633248ff2da07aea27
ELKAISSI SOUHAIL <elkaissimath@gmail.com>
2017-04-25 12:26:13 +0100

    v0.09

--- shanks.c
+++ shanks.c
@@ -4,10 +4,10 @@ int main(int argc,char *argv[])
 {
      char *commande = readFromFile(commande,argv[1]);
      
-   Space *sp = new_Space("souhail");
+   
    Commande *cmd = new_Commande(commande);
     cmd = Commande_interpreter(cmd);
-   sp  = Space_delete(sp);
+   
 
     return 0;
 }
